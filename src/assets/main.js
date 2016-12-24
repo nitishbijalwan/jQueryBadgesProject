b1c55362277faede24c7caab42e9e0dab2061d42
nitish bijalwan <nitishbijalwan01@gmail.com>
2016-12-25 04:08:32 +0530

    Requriement added.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
-$(function() {
+$(function () {
 
   // your code will go here
 
+  $.ajax({
+    
+    url: "https://www.codeschool.com/users/2408022.json",
+    dataType: "jsonp",
+    success: function (response) {
+      var completed=response.courses.completed;
+      for (var index = 0; index < completed.length; index++) {
+        var course = completed[index];
+        console.log(course);
+        $("#badges").append("<div class='course'>"+
+                              "<h3>"+course.title+"</h3>"+
+                              "<img src='"+course.badge+"'/>"+
+                              "<a target='_blank' class='btn btn-primary' href='"+course.url+"'>See Course</a>"+
+                            "</div>");
+      }
+    }
+  });
 });
